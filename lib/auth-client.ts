c679e001cab8f60f161b77016e2d6e26088d5138
AJAmit17 <amitjagadeesh2004@gmail.com>
2025-12-18 00:06:14 +0530

    idk

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NETXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: process.env.NETXT_PUBLIC_BETTER_AUTH_URL || "https://phantom-agent-cli.vercel.app",
 });
 
 export const { 
